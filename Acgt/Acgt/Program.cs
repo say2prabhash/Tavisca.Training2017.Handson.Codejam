11a8cd77ee74d4a96f8994f8760be015599a4e56
say2prabhash <pmohanty@tavisca.com>
2017-07-17 19:24:43 +0530

    Add files via upload

---
+++ Acgt/Acgt/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace CodeJam
+{
+    class ACGT
+    {
+       // private static string consoleInput = "1999|ACGTCCCTTCCCTTCAGAGAGGAGAGAGAGTTCATATATATAATATGGCC,ACCCCGCGCGCGCCTCTCTCTCTCTCTCTCTCCCCCACCCACCACACCAC,ACCCCTCTCTCTCTCTCCGAGAGAGAGGAGAGAGAGGAGGAGCGCGCGCG,CGCGCGCGCGCGCGCGCGCGCGGGGGGGAGGGAGGAGGAGGAGGAGGGTG,TGCGCGCGCGCGCGCGCGCCGGAGAGAGAGAGAGAGAGAGAGGAGAGAGG,TCTCTCTCTCTCGGGAGAGAGGAGGAGGAGGAGGAGGAGAGTCTTGTGTG,TGCGCGGGAGGAGGAGGAGGAGGAGGGGGCGGGGCGGCGCGCGCGCGCGG,AAAATATATATATTATATATATTATATGTGTGTTGGCGCGCGCGCGCGGC,TGTTCGCTCTGCTCGCTCGCTCGCTCGCTCGCTCGCTGCTCTCTCTCTCT,AGAGAGAGAGAGAGAGAGAGAGAGAGAGAGATGGAGAGAGAGAGAGGAGA,TGCGCGCGCGCGCGCGGGGGGAGGAGAGGGTTTTTTTTTTTTTTTTTTTT,GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGCCCCCCCCCCC,GGGGGGGGGGGGGGGGGGGGGGGGGGGGCCCCCCCCCCAAAAAAAAAAAA,TTTTTTTTTTTTTTTTTTTTTTTTTTGGGGGGGGGGGGGGGGAAAAAAAA,GGGTTTTTTTTTTATTATATATATATATATTATATTATATATATATTATT,TGGGGGGGGGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCG,TGCGCTGCTGCTGCTGCTGCTGCTGCTGCTGGGGGGGGGGGGGGGGGGGG,ACGTCCCTTCCCTTCAGAGAGGAGAGAGAGTTCATATATATAATATGGCC,ACCCCGCGCGCGCCTCTCTCTCTCTCTCTCTCCCCCACCCACCACACCAC,ACCCCTCTCTCTCTCTCCGAGAGAGAGGAGAGAGAGGAGGAGCGCGCGCG,CGCGCGCGCGCGCGCGCGCGCGGGGGGGAGGGAGGAGGAGGAGGAGGGTG,TGCGCGCGCGCGCGCGCGCCGGAGAGAGAGAGAGAGAGAGAGGAGAGAGG,TCTCTCTCTCTCGGGAGAGAGGAGGAGGAGGAGGAGGAGAGTCTTGTGTG,TGCGCGGGAGGAGGAGGAGGAGGAGGGGGCGGGGCGGCGCGCGCGCGCGG,AAAATATATATATTATATATATTATATGTGTGTTGGCGCGCGCGCGCGGC,TGTTCGCTCTGCTCGCTCGCTCGCTCGCTCGCTCGCTGCTCTCTCTCTCT,AGAGAGAGAGAGAGAGAGAGAGAGAGAGAGATGGAGAGAGAGAGAGGAGA,TGCGCGCGCGCGCGCGGGGGGAGGAGAGGGTTTTTTTTTTTTTTTTTTTT,GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGCCCCCCCCCCC,GGGGGGGGGGGGGGGGGGGGGGGGGGGGCCCCCCCCCCAAAAAAAAAAAA,TTTTTTTTTTTTTTTTTTTTTTTTTTGGGGGGGGGGGGGGGGAAAAAAAA,GGGTTTTTTTTTTATTATATATATATATATTATATTATATATATATTATT,TGGGGGGGGGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCG,TGCGCTGCTGCTGCTGCTGCTGCTGCTGCTGGGGGGGGGGGGGGGGGGGG,ACGTCCCTTCCCTTCAGAGAGGAGAGAGAGTTCATATATATAATATGGCC,ACCCCGCGCGCGCCTCTCTCTCTCTCTCTCTCCCCCACCCACCACACCAC,ACCCCTCTCTCTCTCTCCGAGAGAGAGGAGAGAGAGGAGGAGCGCGCGCG,CGCGCGCGCGCGCGCGCGCGCGGGGGGGAGGGAGGAGGAGGAGGAGGGTG,TGCGCGCGCGCGCGCGCGCCGGAGAGAGAGAGAGAGAGAGAGGAGAGAGG,TCTCTCTCTCTCGGGAGAGAGGAGGAGGAGGAGGAGGAGAGTCTTGTGTG,TGCGCGGGAGGAGGAGGAGGAGGAGGGGGCGGGGCGGCGCGCGCGCGCGG,AAAATATATATATTATATATATTATATGTGTGTTGGCGCGCGCGCGCGGC,TGTTCGCTCTGCTCGCTCGCTCGCTCGCTCGCTCGCTGCTCTCTCTCTCT,AGAGAGAGAGAGAGAGAGAGAGAGAGAGAGATGGAGAGAGAGAGAGGAGA,TGCGCGCGCGCGCGCGGGGGGAGGAGAGGGTTTTTTTTTTTTTTTTTTTT,GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGCCCCCCCCCCC,GGGGGGGGGGGGGGGGGGGGGGGGGGGGCCCCCCCCCCAAAAAAAAAAAA,TTTTTTTTTTTTTTTTTTTTTTTTTTGGGGGGGGGGGGGGGGAAAAAAAA,GGGTTTTTTTTTTATTATATATATATATATTATATTATATATATATTATT,TGGGGGGGGGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCGCG";
+
+        int MinChanges(int maxPeriod, string[] acgt)
+        {
+            String s = "";
+            for (int i = 0; i < acgt.Length; i++)
+            {
+                s += acgt[i];
+            }
+           // Console.WriteLine(s.Length);
+            
+            int replacement = 0;
+           
+            int []res = new int[maxPeriod];
+            
+                for (int i = 1; i <= maxPeriod; i++)
+                {
+                    replacement = 0;
+                    for (int j = 0; j < i; j++)
+                    {
+                    int[] a = new int[4];
+                   
+                    int max = 1;
+                        for (int k = j; k < s.Length; k += i)
+                        {
+                            if (s[k] == 'A')
+                            {
+                                a[0]++;
+                            }
+                            else if (s[k] == 'C')
+                            {
+                                a[1]++;
+                            }
+                            else if (s[k] == 'G')
+                            {
+                                a[2]++;
+                            }
+                            else
+                            {
+                                a[3]++;
+                            }
+                        }
+                    max = a[0];
+                        for (int m = 1; m < a.Length; m++)
+                        {
+                                
+                                max = Math.Max(max, a[m]);
+                            
+                        }
+                        int temp= (a[0] + a[1] + a[2] + a[3]) - max;
+                    replacement = (replacement + temp);
+
+
+                    }
+                    res[i-1] = replacement;
+                }
+               
+               
+            
+            int resPeriod = res[0];
+            for (int i = 1; i < res.Length; i++)
+            {
+               
+                    resPeriod = Math.Min(resPeriod, res[i]);
+                
+            }
+            return resPeriod;
+        }
+
+        #region Testing code Do not change
+        public static void Main(String[] args)
+        {
+            ACGT aCGT = new ACGT();
+            String input =Console.ReadLine();
+            do
+            {
+                var inputParts = input.Split('|');
+                int maxPeriod = int.Parse(inputParts[0]);
+                string[] acgt = inputParts[1].Split(',');
+                Console.WriteLine(aCGT.MinChanges(maxPeriod, acgt));
+                input = Console.ReadLine();
+            } while (input != "-1");
+        }
+        #endregion
+    }
+}
---
+++ Acgt/Acgt/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Acgt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Acgt")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ca6bc28c-4598-4830-a943-af6ab54051aa")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
